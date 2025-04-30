8f68c372416ec3e62b705eea4fb9f58ec21537e7
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-04-30 10:33:40 +0000

    some stuff on variables and values

--- complete-guide/app.go
+++ complete-guide/app.go
@@ -4,8 +4,33 @@ import "fmt"
 
 func main() {
 	hell_world()
+	values()
+	vars()
 }
 
 func hell_world() {
 	fmt.Print("Hello, world!")
+}
+
+func values() {
+	// Some basic values.
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+func vars() {
+	var a = "inital"
+	fmt.Println(a)
+
+	var b, c = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	f := "Apple" // := operator is only accessable inside of functions
+	fmt.Println(f)
 }
